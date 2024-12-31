76260e020650c40aa190cba29db685cfc9fb1213
MrLightful <romatallinn@gmail.com>
2024-12-31 15:24:56 +0100

    Improve Oauth2AccessTokenProvider in Common

--- php/Common/src/Auth/Oauth2AccessTokenProvider.php
+++ php/Common/src/Auth/Oauth2AccessTokenProvider.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace DizcontoPay\Common\src\Auth;
+namespace DizcontoPay\Common\Auth;
 
 use Http\Promise\Promise;
-use Microsoft\Kiota\Abstractions\RequestAdapter;
 use Microsoft\Kiota\Abstractions\Authentication\AccessTokenProvider;
 use Microsoft\Kiota\Abstractions\Authentication\AllowedHostsValidator;
+
+define('DIZCONTO_OAUTH2_TOKEN_URL', 'https://auth.dizconto.com/oauth/v2/token');
 
 class Oauth2AccessTokenProvider implements AccessTokenProvider
 {
@@ -13,25 +14,29 @@ class Oauth2AccessTokenProvider implements AccessTokenProvider
     private string $clientSecret;
     private ?string $accessToken = null;
 
-    private RequestAdapter $requestAdapter;
-
-    public function __construct(string $clientId, string $clientSecret, RequestAdapter $requestAdapter)
+    public function __construct(string $clientId, string $clientSecret)
     {
         $this->clientId = $clientId;
         $this->clientSecret = $clientSecret;
-        $this->$requestAdapter = $requestAdapter;
     }
 
     public function getAuthorizationTokenAsync(string $url, array $additionalAuthenticationContext = []): Promise
     {
         if ($this->accessToken === null) {
-            // TODO
-            $data = [
-                'grant_type' => 'client_credentials',
-                'client_id' => $this->clientId,
-                'client_secret' => $this->clientSecret
-            ];
-            $this->accessToken = '23123';
+//            $data = [
+//                'grant_type' => 'client_credentials',
+//                'client_id' => $this->clientId,
+//                'client_secret' => $this->clientSecret
+//            ];
+//            $curlHandle = curl_init(DIZCONTO_OAUTH2_TOKEN_URL);
+//            curl_setopt($curlHandle, CURLOPT_POST, true);
+//            curl_setopt($curlHandle, CURLOPT_POSTFIELDS, http_build_query($data));
+//            curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+//            $response = curl_exec($curlHandle);
+//            curl_close($curlHandle);
+//            $response = json_decode($response, true);
+//            $this->accessToken = $response['access_token'];
+            $this->accessToken = 'token';
         }
         return $this->accessToken;
     }
